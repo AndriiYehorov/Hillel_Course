fcfbefe5052fb851d3e565ccb4ad5f0ea4b65f22
AndtiiYehorov <4uvachok95@gmail.com>
2023-09-03 13:05:59 +0300

    Add project ReverseReading

---
+++ Lesson_2/ReverseReading/Program.cs
@@ -0,0 +1,12 @@
+﻿
+int num = 629;
+
+int num1 = num % 10;    // последняя цыфра числа
+int num_temp = num / 10; // число без последней цыфры 
+int num2 = num_temp % 10; // вторая цыфра числа
+num_temp = num_temp / 10;
+int num3 = num_temp % 10; // первая цыфра числа
+Console.Write(num1);
+Console.Write(num2);
+Console.Write(num3);
+Console.ReadKey();
