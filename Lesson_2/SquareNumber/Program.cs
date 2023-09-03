f1f3e4fb74cca6f678dcbf0ea5f90fa47b4ffc1e
AndtiiYehorov <4uvachok95@gmail.com>
2023-09-03 13:29:17 +0300

    Removed redundant variable declaration

--- Lesson_2/SquareNumber/Program.cs
+++ Lesson_2/SquareNumber/Program.cs
@@ -1,5 +1,4 @@
-﻿float num;
-Console.WriteLine("Введите число для возведения в квадрат: ");
+﻿Console.WriteLine("Введите число для возведения в квадрат: ");
 string input = Console.ReadLine();
 float EnteredNumber = float.Parse(input);
 Console.WriteLine("Квадрат числа равен: " + EnteredNumber * EnteredNumber);
