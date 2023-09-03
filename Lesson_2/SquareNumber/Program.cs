3a937522733733eadef239f370fc319e0435cc98
AndtiiYehorov <4uvachok95@gmail.com>
2023-09-03 13:27:35 +0300

    Wait handler connected

--- Lesson_2/SquareNumber/Program.cs
+++ Lesson_2/SquareNumber/Program.cs
@@ -1,11 +1,6 @@
-﻿internal class Program
-{
-    private static void Main(string[] args)
-    {
-        float num;
-        Console.WriteLine("Введите число для возведения в квадрат: ");
-        string input = Console.ReadLine();
-        float EnteredNumber = float.Parse(input);
-        Console.WriteLine("Квадрат числа равен: " + EnteredNumber * EnteredNumber);
-    }
-}
+﻿float num;
+Console.WriteLine("Введите число для возведения в квадрат: ");
+string input = Console.ReadLine();
+float EnteredNumber = float.Parse(input);
+Console.WriteLine("Квадрат числа равен: " + EnteredNumber * EnteredNumber);
+Console.ReadKey();
