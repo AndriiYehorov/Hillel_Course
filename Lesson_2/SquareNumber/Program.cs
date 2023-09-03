67465949d1a5ddab046de2340014e6302d406343
AndtiiYehorov <4uvachok95@gmail.com>
2023-09-03 13:24:19 +0300

    Add project SquareNumber

---
+++ Lesson_2/SquareNumber/Program.cs
@@ -0,0 +1,11 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        float num;
+        Console.WriteLine("Введите число для возведения в квадрат: ");
+        string input = Console.ReadLine();
+        float EnteredNumber = float.Parse(input);
+        Console.WriteLine("Квадрат числа равен: " + EnteredNumber * EnteredNumber);
+    }
+}
