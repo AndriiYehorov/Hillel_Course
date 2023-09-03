8b906e99de71adf1fea530e2f3e65eb94243deb0
AndtiiYehorov <4uvachok95@gmail.com>
2023-09-03 13:08:05 +0300

    Translation of comments into English

--- Lesson_2/ReverseReading/Program.cs
+++ Lesson_2/ReverseReading/Program.cs
@@ -1,11 +1,11 @@
 ﻿
 int num = 629;
 
-int num1 = num % 10;    // последняя цыфра числа
-int num_temp = num / 10; // число без последней цыфры 
-int num2 = num_temp % 10; // вторая цыфра числа
+int num1 = num % 10;    // last digit of the number
+int num_temp = num / 10; // number without last digit 
+int num2 = num_temp % 10; // second digit of the number
 num_temp = num_temp / 10;
-int num3 = num_temp % 10; // первая цыфра числа
+int num3 = num_temp % 10; // first digit of the number
 Console.Write(num1);
 Console.Write(num2);
 Console.Write(num3);
