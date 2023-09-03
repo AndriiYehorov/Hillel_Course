814b30b301aa7ce333742fd57e97fe150c55cdf9
AndtiiYehorov <4uvachok95@gmail.com>
2023-09-03 17:57:00 +0300

    Add project ReorderingOfNumbers

---
+++ Lesson_2/ReorderingOfNumbers/Program.cs
@@ -0,0 +1,11 @@
+﻿int num = 319;
+
+int num1 = num % 10;    // last digit of the number
+int num_temp = num / 10; // number without last digit 
+int num2 = num_temp % 10; // second digit of the number
+num_temp = num_temp / 10;
+int num3 = num_temp % 10; // first digit of the number
+Console.Write(num2);
+Console.Write(num3);
+Console.Write(num1);
+Console.ReadKey();
