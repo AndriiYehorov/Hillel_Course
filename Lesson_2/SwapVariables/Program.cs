7145d7ed1b97c394d569fda91237d73c49d533a7
AndtiiYehorov <4uvachok95@gmail.com>
2023-09-04 11:09:23 +0300

    Added second variant

--- Lesson_2/SwapVariables/Program.cs
+++ Lesson_2/SwapVariables/Program.cs
@@ -1,9 +1,25 @@
-﻿// способы поменять местами переменные, не включая способы рассмотренные на уроке
-byte a = 3;
-byte b = 5;
+﻿using System;
 
-byte c = (byte) (a * b);
-a = (byte) (c / a);
-b = (byte) (c / b);
-Console.WriteLine(a);
-Console.WriteLine(b);
+byte num1 = 3;
+byte num2 = 5;
+Console.WriteLine("Первый метод!");
+Console.Write(num1);
+Console.WriteLine(num2);
+//1
+byte temp = (byte) (num1 * num2);
+num1 = (byte) (temp / num1);
+num2 = (byte) (temp / num2);
+Console.Write(num1);
+Console.WriteLine(num2);
+
+//2
+byte num3 = 6;
+byte num4 = 5;
+Console.WriteLine("Второй метод!");
+Console.Write(num3);
+Console.WriteLine(num4);
+(num3, num4) = (num4, num3); //Использование кортежей, не очень понятно что это, но такой вариант есть
+Console.Write(num3);
+Console.WriteLine(num4);
+
+Console.ReadKey();
