ebdf25108d7ea562195473b6911550a608d8e979
AndtiiYehorov <4uvachok95@gmail.com>
2023-09-04 10:22:41 +0300

    Add project SwapVariables

---
+++ Lesson_2/SwapVariables/Program.cs
@@ -0,0 +1,9 @@
+﻿// способы поменять местами переменные, не включая способы рассмотренные на уроке
+byte a = 3;
+byte b = 5;
+
+byte c = (byte) (a * b);
+a = (byte) (c / a);
+b = (byte) (c / b);
+Console.WriteLine(a);
+Console.WriteLine(b);
