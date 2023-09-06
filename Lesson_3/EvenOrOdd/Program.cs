8732e89cda8538948d36e22f465bf8af5a111e23
AndtiiYehorov <4uvachok95@gmail.com>
2023-09-06 20:03:06 +0300

    Add project EventOrOdd

---
+++ Lesson_3/EvenOrOdd/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace EvenOrOdd
+{
+class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = GettingDataFromUser();
+            
+            CalculateEvenOdd(result);
+            Console.ReadKey();
+        }
+
+    static int GettingDataFromUser()
+    {
+        Console.WriteLine("Введи число: ");
+        
+        string input = Console.ReadLine();
+        if (input == "")
+        {
+            Console.WriteLine("Ты что-то делаешь не так!");
+            GettingDataFromUser();
+        }
+        int num = int.Parse(input);
+        return num;
+    }
+
+    static void CalculateEvenOdd(int num)
+        {
+            int result = num % 2;
+
+            if (result == 0)
+            {
+                Console.WriteLine("Парне");
+            }
+            else
+            {
+                Console.WriteLine("Непарне");
+            }
+        }
+    }
+}
