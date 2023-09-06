c6ea5c832183fd2f9ebdfc23bc8ceade62a13820
AndtiiYehorov <4uvachok95@gmail.com>
2023-09-06 20:36:10 +0300

    Add project MoreOrLess

---
+++ Lesson_3/MoreOrLess/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace MoreOrLess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string result = ExecuteComparison();
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+
+        static string ExecuteComparison()
+        {
+            int num1 = GettingDataFromUser("Введи первое число: ");
+            int num2 = GettingDataFromUser("Введи второе число: ");
+
+            string answer = CompareNumbers(num1,num2);
+
+            return answer;
+        }
+
+        private static string CompareNumbers(int firstNumber, int secondNumber)
+        {
+            if (firstNumber < secondNumber)
+            {
+                //первое число больше
+            }
+            else if (firstNumber > secondNumber)
+            {
+                //второе число больше
+            }
+            else
+            {
+                //числа равны
+            }
+            return "";
+        }
+
+        static int GettingDataFromUser(string messege)
+    {
+            Console.WriteLine(messege);
+            string input = Console.ReadLine();
+            int num = int.Parse(input);
+            return num;
+        }
+    }
+}
