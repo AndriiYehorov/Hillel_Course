494c498b04aac41c9a447762ebdaaaf4dd769251
AndtiiYehorov <4uvachok95@gmail.com>
2023-09-10 15:38:14 +0300

    Add project GuessThePassword

---
+++ Lesson_4/GuessThePassword/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.Design;
+
+namespace GuessThePassword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool passTrue = PasswordCheck(false);
+            if (passTrue)
+            {
+                Console.WriteLine("Добро пожаловать!");
+            }
+            Console.ReadKey();
+        }
+
+        static bool PasswordCheck(bool passTrue)
+        {
+            string pass;
+            while (!passTrue) 
+            {
+                Console.WriteLine("Введи пароль: ");
+                pass = Console.ReadLine();
+                if (pass=="root") 
+                    passTrue = true;    
+            }
+            return passTrue;
+        }
+
+    }
+}
