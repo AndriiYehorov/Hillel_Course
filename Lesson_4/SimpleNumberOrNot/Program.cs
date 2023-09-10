3a9abfe75bffb3ba5b8050fed2a23755f3f27b9d
AndtiiYehorov <4uvachok95@gmail.com>
2023-09-10 17:13:08 +0300

    Add project SimpleNumberOrNot

---
+++ Lesson_4/SimpleNumberOrNot/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace SimpleNumberOrNot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 0;
+            string input= Console.ReadLine();
+            bool result = int.TryParse(input, out num);
+            if (!result) 
+            {
+                Console.WriteLine("Необходимо ввести число!");
+                return;
+            }
+            Console.WriteLine(CheckNum(num));
+            Console.ReadKey();
+        }
+
+        private static string CheckNum(int genNum)
+        {
+            for (int i = 2; i < genNum; i++)
+            {
+                if (genNum % i == 0)
+                {
+                    return "Not simple!";
+                }
+            }
+            return "Simple!";
+        }
+    }
+}
