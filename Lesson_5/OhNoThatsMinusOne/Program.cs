c305cc7a32dc7bcf3e34e751a8d44fcb9d172a20
AndtiiYehorov <4uvachok95@gmail.com>
2023-09-13 22:36:05 +0300

    Add project OhNoThatsMinusOne

---
+++ Lesson_5/OhNoThatsMinusOne/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+
+namespace OhNoThatsMinusOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[] {43, 6, 45, -4, 16, 2, -1, 8, 21, 15};
+            RunCheck(nums);
+        }
+
+        private static void RunCheck(int[] nums)
+        {
+            foreach (int el in nums) 
+            { 
+                if (el != -1)
+                {
+                    Console.WriteLine(el);
+                } else 
+                {
+                    Console.WriteLine("О нет!!! Минус один!");
+                    break;
+                }
+            }
+        }
+    }
+}
