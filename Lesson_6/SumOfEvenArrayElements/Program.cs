727eea045429a0dd3900cab6a4a20d1c17ce7f6f
AndtiiYehorov <4uvachok95@gmail.com>
2023-09-16 22:01:34 +0300

    Add project SumOfEvenArrayElements

---
+++ Lesson_6/SumOfEvenArrayElements/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace SumOfEvenArrayElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[10];
+            FillArrayRandom(nums,151);
+            DisplayMessage("Собственно массив: ",nums);
+            int res = SumOfEvenArrayElements(nums);
+            Console.WriteLine("Сумма значений по положительным индексам = {0}",res);
+        }
+
+        private static int SumOfEvenArrayElements(int[] nums)
+        {
+            int Summ = 0;
+            for (int i = 0; i< nums.Length; i++)
+            {
+                if (i%2 == 0)
+                {
+                    Summ += nums[i];
+                }
+            }
+            return Summ;
+        }
+
+        private static void FillArrayRandom(int[] nums, int maxRand)
+        {
+            Random random = new Random();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                nums[i] = random.Next(maxRand);
+            }
+        }
+
+        private static void DisplayMessage(string message, int[] arr)
+        {
+            Console.WriteLine(message);
+            foreach (var el in arr)
+            {
+                Console.Write(el + "\t");
+            }
+            Console.WriteLine();
+        }
+        
+    }
+}
