193fd73cb14546b11fd4ae74204fbb9e115ce5d6
AndtiiYehorov <4uvachok95@gmail.com>
2023-09-18 00:13:10 +0300

    Add project SortingLastNames

---
+++ Lesson_6/SortingLastNames/Program.cs
@@ -0,0 +1,42 @@
+﻿
+namespace SortingLastNames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DisplayText("Заполни массив фамилиями!");
+            string[] lastNames = ReceiveDataFromUser();
+            Array.Sort(lastNames);
+            DisplayText("Отсортированный массив : ");
+            PrintArray(lastNames);
+        }
+
+        private static void DisplayText(string text)
+        {
+            Console.WriteLine(text);
+        }
+
+        private static string[] ReceiveDataFromUser()
+        {
+            string[] lastNames = new string[5];
+            for (int i = 0; i < lastNames.Length; i++)
+            {
+                lastNames[i] = Console.ReadLine();
+            }
+            DisplayText("Неотсортированный массив : ");
+            PrintArray(lastNames);
+            return lastNames;
+        }
+
+
+        private static void PrintArray(string[] str)
+        {
+            foreach (string el in str) 
+            {
+                Console.Write(el+"\t"); 
+            }
+            Console.WriteLine();
+        }
+    }
+}
