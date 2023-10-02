ab7cf2da5a955c12e9ad32d277e8733f8e3c20a8
AndtiiYehorov <4uvachok95@gmail.com>
2023-10-02 15:13:03 +0300

    Add project UserInformation

---
+++ Lesson_9/UserInformation/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace UserInformation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GetAndDisplayDataFromUser();
+        }
+
+        private static void GetAndDisplayDataFromUser()
+        {
+            string name, surname, login;
+            int age;
+            bool res;
+            int result;
+            Console.Write("Ведите свое имя: ");
+            name = Console.ReadLine();
+            Console.Write("Введите свою фамилию: ");
+            surname = Console.ReadLine();
+            Console.Write("Введите логин: ");
+            login = Console.ReadLine();
+            do
+            {
+                Console.Write("Ведите свой возраст: ");
+                string index = Console.ReadLine();
+                res = int.TryParse(index, out result);
+            } while (!res);
+            age = result;
+            User myUser = new User(login,name,surname,age);
+            myUser.Messege();
+        }
+
+    }
+}
---
+++ Lesson_9/UserInformation/User.cs
@@ -0,0 +1,49 @@
+﻿namespace UserInformation
+{
+    internal class User
+    {
+        private string login;
+        public string Login
+        {
+            get { return login; }
+            set { login = value; }
+        }
+
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        
+        private string surname;
+        public string Surname
+        {
+            get { return surname; }
+            set { surname = value; }
+        }
+        
+        private int age;
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+
+        DateTime date { get; }
+
+        public User(string login, string name, string surname, int age)
+        {
+            date = DateTime.Now;
+            this.login = login;
+            this.name = name;
+            this.surname = surname;
+            this.age = age;
+        }
+
+        public void Messege() 
+        {
+            Console.WriteLine("Анкета пользователя: \n1.Имя: {0}\n2.Фамилия: {1}\n3.Логин: {2}\n4.Возраст: {3}\n5.Дата создания: {4}", this.name,this.surname,this.login,this.age,this.date);
+        }
+    }
+}
