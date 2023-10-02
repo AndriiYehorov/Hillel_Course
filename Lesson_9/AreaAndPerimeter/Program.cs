e223c7ad1f613267ab1358d7adaf46835aa67619
AndtiiYehorov <4uvachok95@gmail.com>
2023-10-02 14:00:51 +0300

    Add project AreaAndPerimeter

---
+++ Lesson_9/AreaAndPerimeter/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace AreaAndPerimeter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = SidesOfTheRectangle("Введите первую сторону прямоугольника: ");
+            double b = SidesOfTheRectangle("Введите вторую сторону прямоугольника: ");
+            Rectangle rectangle = new Rectangle(a, b);
+            Console.WriteLine("Периметри прямоугольника равен: {0}, а площадь равна {1}", rectangle.PerimeterCalculator(),rectangle.AreaCalculator());
+        }
+
+        private static double SidesOfTheRectangle(string text)
+        {
+            double result;
+            bool res = false;
+            Console.WriteLine(text);
+            do
+            {
+                string input = Console.ReadLine();
+                res = double.TryParse(input, out result);
+            } while (!res);
+            return result;
+        }
+    }
+}
---
+++ Lesson_9/AreaAndPerimeter/Rectangle.cs
@@ -0,0 +1,27 @@
+﻿namespace AreaAndPerimeter
+{
+    internal class Rectangle
+    {
+        double side1;
+        double side2;
+
+        public double Area { get; }
+        public double Perimeter { get; }
+
+        public Rectangle(double side1, double side2)
+        {
+            this.side1 = side1;
+            this.side2 = side2;
+        }
+
+        public double AreaCalculator()
+        {
+            return this.side1*this.side2;
+        }
+
+        public double PerimeterCalculator()
+        {
+            return (this.side1+this.side2)*2;
+        }
+    }
+}
