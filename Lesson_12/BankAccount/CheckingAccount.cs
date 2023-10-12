c3e32a3442321e0529d1fb9009a313edc7fd3fcc
AndtiiYehorov <4uvachok95@gmail.com>
2023-10-12 22:29:31 +0300

    Add project BankAccount

---
+++ Lesson_12/BankAccount/CheckingAccount.cs
@@ -0,0 +1,37 @@
+﻿namespace BankAccount
+{
+    internal class CheckingAccount : BankAccount
+    {
+        double overdraftLimit;
+        public CheckingAccount(string accountHolder, double balance, double overdraftLimit) : base(accountHolder, balance)
+        {
+            this.overdraftLimit = overdraftLimit;
+        }
+        public override double Deposit(double amount)
+        {
+            balance = balance+amount;
+            return balance;
+        }
+
+        public override void DisplayAccountInfo()
+        {
+            Console.WriteLine($"Balance = {balance}");
+            Console.WriteLine($"AccountHolder = {accountHolder}");
+            Console.WriteLine($"AccountNumber = {accountNumber}");
+            Console.WriteLine($"overdraftLimit = {overdraftLimit}");
+        }
+
+        public override double Withdraw(double amount)
+        {
+            if (amount <= (balance + overdraftLimit))
+            {
+                balance -= amount;
+            }
+            else
+            {
+                Console.WriteLine("limit is exceeded");
+            }
+            return balance;
+        }
+    }
+}
---
+++ Lesson_12/BankAccount/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace BankAccount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount bankSavingsAccount = new SavingsAccount("Me", 5000, 0.05);
+
+            bankSavingsAccount.DisplayAccountInfo();
+            Console.WriteLine();
+            bankSavingsAccount.Withdraw(1000);
+            bankSavingsAccount.DisplayAccountInfo();
+            Console.WriteLine();
+            bankSavingsAccount.Deposit(8000);
+            bankSavingsAccount.DisplayAccountInfo();
+            Console.WriteLine();
+
+            BankAccount bankSavingsAccount_2 = new SavingsAccount("You", 8000, 0.05);
+            bankSavingsAccount_2.DisplayAccountInfo();
+            Console.WriteLine();
+
+            BankAccount bankCheckingAccount = new CheckingAccount("Friend", 12500, 10000);
+            bankCheckingAccount.DisplayAccountInfo();
+            Console.WriteLine();
+            bankCheckingAccount.Withdraw(19500);
+            bankCheckingAccount.DisplayAccountInfo();
+            Console.WriteLine();
+            bankCheckingAccount.Deposit(9000);
+            bankCheckingAccount.DisplayAccountInfo();
+            Console.WriteLine();
+            bankCheckingAccount.Withdraw(13000);
+            bankCheckingAccount.DisplayAccountInfo();
+
+
+
+
+        }
+    }
+}
