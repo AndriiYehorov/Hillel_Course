e77299649222eea02bb8c69860d0b655f363f10f
AndtiiYehorov <4uvachok95@gmail.com>
2023-10-16 09:36:55 +0300

    Add project MathUtility

---
+++ Lesson_13/MathUtility/MathUtility.cs
@@ -0,0 +1,37 @@
+﻿namespace MathUtility
+{
+    internal static class MathUtility
+    {
+        public static int Addition(int x,int y)
+        {
+            return x+y;
+        }
+        public static int Subtraction(int x, int y) 
+        {  
+            return x-y; 
+        }
+        public static int Multiplication(int x, int y) 
+        {  
+            return x*y; 
+        }
+        public static double Division(int x, int y) 
+        {
+            try
+            {
+                return x/y;
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Деление на ноль!");
+                return 0;
+            }
+            catch
+            {
+                Console.WriteLine("Возникло исключение");               
+            }
+            return 0;
+        }
+
+
+    }
+}
---
+++ Lesson_13/MathUtility/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace MathUtility
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathUtility.Addition(1,2);
+            MathUtility.Subtraction(1, 2);
+            MathUtility.Multiplication(1, 2);
+            MathUtility.Division(1, 2);
+        }
+    }
+}
